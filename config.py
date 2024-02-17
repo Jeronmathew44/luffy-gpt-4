e23463dfd4471fb5507522c6476fd66643e29b3e
NARUTO <150582863+Naruto46i@users.noreply.github.com>
2024-02-17 15:42:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ load_dotenv()
 
 class Config(object):
     BOT_TOKEN = os.getenv("BOT_TOKEN")
+    SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1002029882658')
     API_ID = os.getenv("API_ID")
     API_HASH = os.getenv("API_HASH")
     mediaPattern = r"\b(https?://(?:(.*?)\.)?(?:instagram\.com|instagr\.am|t\.co|twitter\.com)(?:[^\s]*))\b" #pin\.it|pinterest\.com|
